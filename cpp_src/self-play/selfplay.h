9713a948f3e6541240852dbebba6bdc70718947b
robertleoj <robert.leo.jonsson@gmail.com>
2023-02-23 11:31:01 +0000

    revert threading optimization - it did not work

--- cpp_src/self-play/selfplay.h
+++ cpp_src/self-play/selfplay.h
@@ -29,15 +29,18 @@ public:
 
     void thread_play(
         int thread_idx, 
-        utils::thread_queue<eval_request> * eval_q,
+        std::queue<eval_request> * eval_q,
         // std::mutex * q_mutex,
+        std::mutex * q_mutex,
         std::mutex * db_mutex,
         bool *req_completed,
         std::mutex *req_completed_mutex,
         std::unique_ptr<nn::NNOut> *evaluations,
         std::condition_variable * eval_cv,
+        std::condition_variable * q_cv,
         // std::condition_variable * nn_q_wait_cv    ,
         // std::mutex * results_mutex,
+        std::mutex * results_mutex,
         std::atomic<int> * games_left,
         std::atomic<int> * num_active_threads
     );
