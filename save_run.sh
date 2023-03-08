e044fa20273d55cbb4222740babd30b4d3f37ca2
The big cheese <robert.leo.jonsson@gmail.com>
2023-03-08 22:57:08 +0000

    7z api bad

--- save_run.sh
+++ save_run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-mkdir -p saved_runs
-
-mkdir -p $1
+mkdir -p ./saved_runs/$1
 
 7z a ./saved_runs/$1/db.7z -r ./db/db.db
-7z a ./saved_runs/$1/models.7z -r ./models
+7z a ./saved_runs/$1/models.7z ./models
 
 cp cpp_src/config_files/hyperparameters.json ./saved_runs/$1/hyperparameters.json
 cp py_src/config.py ./saved_runs/$1/config.py
