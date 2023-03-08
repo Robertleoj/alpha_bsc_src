a7365fa8fe9bdaeb7ba7d5f78dc9cff340a13f47
BigBrainMan <bigbrainman@pop-os.localdomain>
2023-03-08 22:39:56 +0000

    copy hyperparameters when saving run

--- save_run.sh
+++ save_run.sh
@@ -6,3 +6,7 @@ mkdir -p $1
 
 7z a ./saved_runs/$1/db.7z -r ./db/db.db
 7z a ./saved_runs/$1/models.7z -r ./models
+
+cp cpp_src/config_files/hyperparameters.json ./saved_runs/$1/hyperparameters.json
+cp py_src/config.py ./saved_runs/$1/config.py
+
