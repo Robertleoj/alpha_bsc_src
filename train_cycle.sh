504283398788731799e13a3a02c6c44b861832f5
Student user <gimli@base>
2023-03-13 22:26:42 +0000

    some new runs

--- train_cycle.sh
+++ train_cycle.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --account=gimli
+#SBATCH --account=staff
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32gb                 
 #SBATCH --partition=beQuick
+#SBATCH --output=logs/train_window.log
 
 
 RUNPATH=/home/gimli/AlphaBSc/alpha_bsc_src
@@ -14,3 +15,6 @@ cd $RUNPATH
 source ./py_src/.venv/bin/activate
 
 python3 train_cycle.py $1 $2
+
+
+
