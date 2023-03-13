a8af599e7f856c0d47c49857eef50aa250b8d2e9
robertleoj <robert.leo.jonsson@gmail.com>
2023-03-13 20:45:25 +0000

    update utility scripts

--- copy_from_smallvoice.sh
+++ copy_from_smallvoice.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <run-name>"
-  exit 1
+# Check if arguments are passed
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <run-name>"
+    exit 1
 fi
 
-mkdir -p ./saved_runs/$1
-scp -r gimli@smallvoice.ru.is:/home/gimli/AlphaBSc/alpha_bsc_src/saved_runs/$1 ./saved_runs/
+RUN_NAME=$1
+
+mkdir -p ./saved_runs/$RUN_NAME
+scp gimli@smallvoice.ru.is:/home/gimli/AlphaBSc/alpha_bsc_src/saved_runs/$RUN_NAME.7z ./saved_runs/
